ce1d1d474a8fd602176cf48943af91b6bb82b49d
Rishi N Desai <rishindesai@yahoo.com>
2020-12-09 20:21:11 +0530

    redis cloud

--- infra/redis.js
+++ infra/redis.js
@@ -1,11 +1,24 @@
 const redis = require("async-redis")
+require('dotenv').config({path: __dirname + '/../.env'})
 
-const redis_url = process.env.REDIS_URL || "redis://127.0.0.1"
+let redisClient
+if(process.env.NODE_ENV === "prod") {
+    redisClient = {
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT
+    }
+}
+else {
+    redisClient = "redis://127.0.0.1"
+}
 
-const client = redis.createClient(redis_url)
+const client = redis.createClient(redisClient)
 
-client.on('error', err => {
-    console.log(err)
-})
+client.auth(process.env.REDIS_PASSWORD, err => {
+    if(err) {
+        throw err;
+    }
+    console.log("redis connected")
+});
 
 module.exports = client
